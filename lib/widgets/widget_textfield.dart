db28acca93131959dcb7a9c51baff2ab9b95c57b
Hiyooooo <hiyokiou@gmail.com>
2025-09-02 17:15:22 +0700

    ref/ [HYO] update-widget-textfield

--- lib/widgets/widget_textfield.dart
+++ lib/widgets/widget_textfield.dart
@@ -4,22 +4,32 @@ class AppTextField extends StatelessWidget {
   final String label;
   final TextEditingController? controller;
   final int maxLines;
+  final bool obscureText;
+  final Widget? suffixIcon;
+  final Widget? prefixIcon;
 
   const AppTextField({
     super.key,
     required this.label,
     this.controller,
     this.maxLines = 1,
+    this.obscureText = false,
+    this.suffixIcon,
+    this.prefixIcon,
   });
 
   @override
   Widget build(BuildContext context) {
     return TextField(
       controller: controller,
-      maxLines: maxLines,
-      decoration: const InputDecoration(
-        border: OutlineInputBorder(),
-      ).copyWith(labelText: label),
+      maxLines: obscureText ? 1 : maxLines,
+      obscureText: obscureText,
+      decoration: InputDecoration(
+        border: const OutlineInputBorder(),
+        labelText: label,
+        prefixIcon: prefixIcon,
+        suffixIcon: suffixIcon,
+      ),
     );
   }
 }
