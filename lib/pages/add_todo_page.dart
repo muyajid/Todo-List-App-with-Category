e83f0f3fe8c69e700a88ce7e89cf63ddfaa2e498
Hiyooooo <hiyokiou@gmail.com>
2025-09-03 13:54:52 +0700

    feat/[HYO] create-floating-action-button

---
+++ lib/pages/add_todo_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AddTodoPage extends StatelessWidget {
+  const AddTodoPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Add Todo')),
+      body: const SizedBox.shrink(),
+    );
+  }
+}
