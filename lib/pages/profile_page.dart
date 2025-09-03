6cec13f00de9609e671724fa97be53f53e011412
Hiyooooo <hiyokiou@gmail.com>
2025-09-03 18:21:08 +0700

    feat/update-profil-page-ui

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -2,7 +2,77 @@ import 'package:flutter/material.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
+
   @override
-  Widget build(BuildContext context) =>
-      const Center(child: Text('Profile Page'));
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Profile")),
+      body: ListView(
+        padding: const EdgeInsets.all(16),
+        children: const [
+          Text(
+            "Data Profil",
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 16),
+          ProfileCard(
+            imagePath: "assets/images/profil1.png",
+            name: "Muhammad Yazid",
+          ),
+          SizedBox(height: 16),
+          ProfileCard(
+            imagePath: "assets/images/profil2.png",
+            name: "Adika Ruzain",
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ProfileCard extends StatelessWidget {
+  final String imagePath;
+  final String name;
+
+  const ProfileCard({super.key, required this.imagePath, required this.name});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 3,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+      child: Padding(
+        padding: const EdgeInsets.all(24),
+        child: Row(
+          children: [
+            CircleAvatar(radius: 40, backgroundImage: AssetImage(imagePath)),
+            const SizedBox(width: 20),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    name,
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  const Text(
+                    "Kelas: 11 PPLG 1",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  const Text(
+                    "SMK Raden Umar Said",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
